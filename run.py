6f11993fdbdaffc4835ebddf5e09221a524d0ede
Kamirus <listopadkamil@gmail.com>
2016-05-15 14:02:47 +0200

    corrected instructions

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 
 url = raw_input("Give me url: ")
-phrase = raw_input("Give me phrase which files contain: ")
+phrase = raw_input("Give me EXACT phrase which file addresses contain: ")
 
 os.system("python download-exe.py " + url + " " + phrase)
